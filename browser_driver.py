b995d7d9a4b614b089fed70df3b678fdac27136b
Rsshiki <jhuiiyuljw@gmail.com>
2025-04-16 14:19:07 +0800

    从edge换成firefox了，edge的目录占用问题解决不了

--- browser_driver.py
+++ browser_driver.py
@@ -1,32 +1,29 @@
-import logging
-from selenium.webdriver.edge.options import Options
-from selenium.webdriver.edge.service import Service
-from selenium import webdriver
-from webdriver_manager.microsoft import EdgeChromiumDriverManager
-import tempfile
 import os
 import shutil
+import logging
+import tempfile
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.service import Service
+from webdriver_manager.firefox import GeckoDriverManager
 
 # 配置 Python 日志，方便调试
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def setup_driver(headless=True): #True为无头模式，False为有头模式
     """设置浏览器驱动"""
-    edge_options = Options()
+    firefox_options = Options()
     if headless:
-        edge_options.add_argument('--headless')
-    edge_options.add_argument('--log-level=3')
-    edge_options.add_experimental_option('excludeSwitches', ['enable-logging'])
-
+        firefox_options.add_argument('-headless')
+    firefox_options.set_preference("general.log.level", "fatal")
     user_data_dir = None
     driver = None
     try:
         user_data_dir = tempfile.mkdtemp()
-        edge_options.add_argument(f'--user-data-dir={user_data_dir}')
-        service = Service(EdgeChromiumDriverManager().install())
-        print("尝试启动浏览器...")
-        driver = webdriver.Edge(service=service, options=edge_options)
-        print("浏览器成功启动！")
+        firefox_options.add_argument(f'-profile')
+        firefox_options.add_argument(user_data_dir)
+        service = Service(GeckoDriverManager().install())
+        driver = webdriver.Firefox(service=service, options=firefox_options)
     except Exception as e:
         logging.error(f"启动浏览器时出错: {e}")
         if user_data_dir and os.path.exists(user_data_dir):
