4f103c321fb21d5cbbc1f910ebc2904a5fd54f44
Rsshiki <jhuiiyuljw@gmail.com>
2025-04-17 18:49:21 +0800

    重新整理了运行逻辑，保证能够获取到下次打工时间，并依据最晚的打工时间创建计划任务

--- sign_work.py
+++ sign_work.py
@@ -1,21 +1,25 @@
 import time
 import random
-from config_handler import save_config, load_config
+import logging
 from datetime import datetime, timedelta
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from config_handler import load_config, save_config
 from selenium.webdriver.support import expected_conditions as EC
 
+# 配置日志
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
 # 签到页面选择器
-NEED_LOGIN_SIGN_XPATH = '//div[@id="messagetext" and contains(@class, "alert_info")]//p[contains(text(), "您需要先登录才能继续本操作")]'
-ALREADY_SIGNED_XPATH = '//h1[@class="mt" and contains(text(), "您今天已经签到过了或者签到时间还未开始")]'
+NEED_LOGIN_SIGN_CSS = '#messagetext.alert_info p'
+ALREADY_SIGNED_CSS = 'h1.mt'
 SMILE_BUTTON_CSS_TEMPLATE = 'ul.qdsmile {}'
 RADIO_BUTTON_CSS = '#qiandao > table.tfm > tbody > tr:nth-child(1) > td > label:nth-child(2) > input[type=radio]'
 SUBMIT_BUTTON_CSS = '#qiandao > table:nth-child(11) > tbody > tr > td > div > a:nth-child(2)'
 
 # 打工页面选择器
-NEED_LOGIN_WORK_XPATH = '//div[@id="messagetext" and contains(@class, "alert_info")]//p[contains(text(), "请先登录再进行点击任务")]'
-NEED_WAIT_WORK_XPATH = '//div[@id="messagetext" and contains(@class, "alert_info")]//p[contains(text(), "必须与上一次间隔6小时0分钟0秒才可再次进行。")]'
+NEED_LOGIN_WORK_CSS = '#messagetext.alert_info p'
+NEED_WAIT_WORK_CSS = '#messagetext.alert_info p'
 STOP_AD_BUTTON_CSS = '#stopad a'
 
 def calculate_work_time(driver):
@@ -24,137 +28,202 @@ def calculate_work_time(driver):
     :param driver: 浏览器驱动
     :return: 上次打工时间和下次打工时间，如果未获取到等待信息则返回 None, None
     """
-    wait_elements = driver.find_elements(By.XPATH, NEED_WAIT_WORK_XPATH)
-    if wait_elements:
-        wait_text = wait_elements[0].text.split('您需要等待')[1].split('后即可进行。')[0]
-        time_parts = {'hours': 0, 'minutes': 0, 'seconds': 0}
-        unit_mapping = {
-            '小时': 'hours',
-            '分钟': 'minutes',
-            '秒': 'seconds'
-        }
-        for unit in unit_mapping:
-            if unit in wait_text:
-                value, wait_text = wait_text.split(unit, 1)
-                time_parts[unit_mapping[unit]] = int(value)
+    # logging.info("开始计算下次打工时间")
+    wait_elements = driver.find_elements(By.CSS_SELECTOR, NEED_WAIT_WORK_CSS)
+    target_text = "必须与上一次间隔6小时0分钟0秒才可再次进行"
+    for element in wait_elements:
+        if target_text in element.text:
+            # logging.info("找到等待信息，开始解析时间")
+            wait_text = element.text.split('您需要等待')[1].split('后即可进行。')[0]
+            time_parts = {'hours': 0, 'minutes': 0, 'seconds': 0}
+            unit_mapping = {
+                '小时': 'hours',
+                '分钟': 'minutes',
+                '秒': 'seconds'
+            }
+            for unit in unit_mapping:
+                if unit in wait_text:
+                    value, wait_text = wait_text.split(unit, 1)
+                    time_parts[unit_mapping[unit]] = int(value)
 
-        last_work_time = datetime.now() - timedelta(hours=6) + timedelta(**time_parts)
-        next_work_time = last_work_time + timedelta(hours=6)
-        return last_work_time, next_work_time
+            last_work_time = datetime.now() - timedelta(hours=6) + timedelta(**time_parts)
+            next_work_time = last_work_time + timedelta(hours=6)
+            # logging.info(f"计算完成，上次打工时间: {last_work_time}, 下次打工时间: {next_work_time}")
+            return last_work_time, next_work_time
+    # logging.info("未找到等待信息，返回 None")
     return None, None
 
 def perform_sign(driver, username, SIGN_URL):
     """执行签到操作"""
-    config = load_config()
+    # logging.info(f"开始执行账号 {username} 的签到操作")
+    # 判断当前时间是否在 0 点到 1 点之间
+    current_hour = datetime.now().hour
+    if 0 <= current_hour < 1:
+        logging.info("当前时间在 0 点到 1 点之间，不允许签到，跳过签到操作")
+        return
+
     driver.get(SIGN_URL)
     # 检查是否需要重新登录
-    if driver.find_elements(By.XPATH, NEED_LOGIN_SIGN_XPATH):
-        print("Cookie过期，标记状态...")
-        if username in config:
-            config[username]["is_cookie_valid"] = False
-            save_config(config)
-        return
+    # logging.info("检查cookies是否过期")
+    login_elements = driver.find_elements(By.CSS_SELECTOR, NEED_LOGIN_SIGN_CSS)
+    target_text = "您需要先登录才能继续本操作"
+    for element in login_elements:
+        if target_text in element.text:
+            logging.info("Cookie过期，标记状态...")
+            config = load_config()
+            if username in config.get('accounts', {}):
+                config['accounts'][username]["is_cookie_valid"] = False
+                save_config(config)
+            return
 
-    # 检查是否已经签到
-    if driver.find_elements(By.XPATH, ALREADY_SIGNED_XPATH):
-        print("今日已签到")
-        return
+    try:
+        # 显式等待检查是否已经签到的元素加载完成
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, ALREADY_SIGNED_CSS))
+        )
+        logging.info("检查是否已经签到")
+        signed_elements = driver.find_elements(By.CSS_SELECTOR, ALREADY_SIGNED_CSS)
+        target_text = "您今天已经签到过了或者签到时间还未开始"
+        for element in signed_elements:
+            if target_text in element.text:
+                logging.info("今日已签到")
+                return
+    except Exception as e:
+        logging.error(f"检查是否已经签到时出错: {e}")
 
     # 执行签到动作
+    logging.info("还未签到，开始执行签到动作...")
     smile_buttons = ['#kx', '#ng', '#ym', '#wl', '#nu', '#ch', '#fd', '#yl', '#shuai']
     random_smile = random.choice(smile_buttons)
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, SMILE_BUTTON_CSS_TEMPLATE.format(random_smile)))).click()
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, RADIO_BUTTON_CSS))).click()
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, SUBMIT_BUTTON_CSS))).click()
-    print("签到成功")
+    try:
+        logging.info(f"尝试点击表情按钮: {random_smile}")
+        WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, SMILE_BUTTON_CSS_TEMPLATE.format(random_smile)))).click()
+        logging.info("表情按钮点击成功")
+    except Exception as e:
+        logging.error(f"点击表情按钮时出错: {e}")
+        return
+
+    try:
+        logging.info("尝试点击单选按钮")
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, RADIO_BUTTON_CSS))).click()
+        logging.info("单选按钮点击成功")
+    except Exception as e:
+        logging.error(f"点击单选按钮时出错: {e}")
+        return
+
+    try:
+        logging.info("尝试点击提交按钮")
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, SUBMIT_BUTTON_CSS))).click()
+        logging.info("提交按钮点击成功，签到完成")
+    except Exception as e:
+        logging.error(f"点击提交按钮时出错: {e}")
+        return
 
 
 def perform_work(driver, username, WORK_URL):
     """执行打工操作"""
-    config = load_config()
+    logging.info(f"开始执行账号 {username} 的打工操作")
     driver.get(WORK_URL)
-    # 检查是否需要重新登录
-    if driver.find_elements(By.XPATH, NEED_LOGIN_WORK_XPATH):
-        print("Cookie过期，标记状态...")
-        if username in config:
-            config[username]["is_cookie_valid"] = False
-            save_config(config)
-        return
 
-    # 检查是否需要等待
-    last_work_time, _ = calculate_work_time(driver)  # 使用下划线忽略未使用的next_work_time
-    if last_work_time is not None:
-        print(f"在{last_work_time.strftime('%H:%M:%S')}已打过工")
-        return
+    # 检查cookies是否过期
+    # logging.info("检查cookies是否过期")
+    login_elements = driver.find_elements(By.CSS_SELECTOR, NEED_LOGIN_WORK_CSS)
+    target_text = "请先登录再进行点击任务"
+    for element in login_elements:
+        if target_text in element.text:
+            logging.info("Cookie过期，标记状态...")
+            config = load_config()
+            if username in config.get('accounts', {}):
+                config['accounts'][username]["is_cookie_valid"] = False
+                save_config(config)
+            return
 
-    while True:
-        # 执行打工动作
-        print("开始执行打工动作")
-        # 从网页获取所有以 np_advid 开头的按钮 id
-        work_buttons = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, '[id^="np_advid"]')))
-        work_buttons_ids = [button.get_attribute('id') for button in work_buttons]
-        random.shuffle(work_buttons_ids)  # 随机打乱按钮顺序
-        print(f"找到的所有打工按钮: {work_buttons_ids}")
+    try:
+        # 检查是否有以 np_advid 开头的按钮
+        work_buttons = WebDriverWait(driver, 3).until(
+            EC.presence_of_all_elements_located((By.CSS_SELECTOR, '[id^="np_advid"]')))
+        if work_buttons:
+            logging.info("找到打工按钮，开始执行打工动作")
+            work_buttons_ids = [button.get_attribute('id') for button in work_buttons]
+            random.shuffle(work_buttons_ids)  # 随机打乱按钮顺序
+            logging.info(f"找到的所有打工按钮: {work_buttons_ids}")
 
-        all_clicked_success = True
-        for button_id in work_buttons_ids:
-            try:
-                button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, button_id)))
-                print(f"成功定位到打工按钮: {button_id}")
-                
-                # 记录当前窗口句柄
-                original_window = driver.current_window_handle
-                
-                button.click()
-                time.sleep(random.uniform(1, 3))
-                
-                # 切换回原窗口
-                driver.switch_to.window(original_window)
-                
-                # 检查按钮是否成功点击
-                a_element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, f'#{button_id} a')))
-                style_value = a_element.get_attribute('style')
-                if style_value and 'display: none;' in ' '.join(style_value.split()).strip():
-                    print(f"按钮 {button_id} 点击成功")
-                else:
-                    print(f"按钮 {button_id} 点击可能未成功")
+            all_clicked_success = True
+            for button_id in work_buttons_ids:
+                try:
+                    # logging.info(f"尝试定位打工按钮: {button_id}")
+                    button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, button_id)))
+                    # logging.info(f"成功定位到打工按钮: {button_id}")
+
+                    original_window = driver.current_window_handle
+                    # logging.info(f"点击打工按钮: {button_id}")
+                    button.click()
+                    time.sleep(random.uniform(1, 3))
+
+                    driver.switch_to.window(original_window) # 切换回原始窗口
+
+                    # logging.info(f"检查按钮 {button_id} 是否点击成功")
+                    a_element = WebDriverWait(driver, 10).until(
+                        EC.presence_of_element_located((By.CSS_SELECTOR, f'#{button_id} a')))
+                    style_value = a_element.get_attribute('style')
+                    if style_value and 'display: none;' in ' '.join(style_value.split()).strip():
+                        logging.info(f"按钮 {button_id} 点击成功")
+                    else:
+                        logging.info(f"按钮 {button_id} 点击可能未成功")
+                        all_clicked_success = False
+
+                except Exception as e:
+                    logging.error(f"定位、点击或检查打工按钮 {button_id} 时出错: {e}")
                     all_clicked_success = False
 
-            except Exception as e:
-                print(f"定位、点击或检查打工按钮 {button_id} 时出错: {e}")
-                all_clicked_success = False
+            if not all_clicked_success:
+                logging.info("部分广告按钮未成功点击")
+                choice = input("部分广告按钮未成功点击，是否重新进行打工？(输入 y 重新打工，其他任意键退出): ")
+                if choice.lower() == 'y':
+                    perform_work(driver, username, WORK_URL)
+                return
 
-        if not all_clicked_success:
-            choice = input("部分广告按钮未成功点击，是否重新进行打工？(输入 y 重新打工，其他任意键退出): ")
-            if choice.lower() != 'y':
-                break
-            continue
-
-        if all_clicked_success:
-            try:
-                stop_ad_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, STOP_AD_BUTTON_CSS)))
-                print("成功定位到停止广告按钮")
-                stop_ad_button.click()
-
-                # 检查是否出现失败提示
+            if all_clicked_success:
                 try:
-                    failure_element = WebDriverWait(driver, 10).until(
-                        EC.presence_of_element_located((By.XPATH, '//div[@id="messagetext" and @class="alert_info"]//p[contains(text(), "不要作弊哦，重新进行游戏吧！")]'))
-                    )
-                    print("打工失败，页面提示需要重新进行游戏。")
-                    choice = input("打工失败，是否重新进行打工？(输入 y 重新打工，其他任意键退出): ")
-                    if choice.lower() != 'y':
-                        break
-                    else:
-                        continue
-                except:
-                    print("打工可能成功，未检测到失败提示信息。")
-                    print("打工动作执行完成")
-                    break
+                    # logging.info("尝试定位停止广告按钮")
+                    stop_ad_button = WebDriverWait(driver, 10).until(
+                        EC.element_to_be_clickable((By.CSS_SELECTOR, STOP_AD_BUTTON_CSS)))
+                    # logging.info("成功定位到停止广告按钮")
+                    logging.info("点击停止广告按钮")
+                    stop_ad_button.click()
 
-            except Exception as e:
-                print(f"定位或点击停止广告按钮时出错: {e}")
-                all_clicked_success = False
-                choice = input("点击停止广告按钮失败，是否重新进行打工？(输入 y 重新打工，其他任意键退出): ")
-                if choice.lower() != 'y':
-                    break
+                    # 检查是否出现失败提示
+                    try:
+                        logging.info("检查是否出现打工失败提示")
+                        possible_elements = WebDriverWait(driver, 10).until(
+                            EC.presence_of_element_located((By.CSS_SELECTOR, '#messagetext.alert_info p'))
+                        )
+                        target_text = "不要作弊哦，重新进行游戏吧！"
+                        failure_element = None
+                        if isinstance(possible_elements, list):
+                            for element in possible_elements:
+                                if target_text in element.text:
+                                    failure_element = element
+                                    break
+                        elif target_text in possible_elements.text:
+                            failure_element = possible_elements
+
+                        if failure_element:
+                            logging.info("打工失败，页面提示需要重新进行游戏。")
+                            choice = input("打工失败，是否重新进行打工？(输入 y 重新打工，其他任意键退出): ")
+                            if choice.lower() == 'y':
+                                perform_work(driver, username, WORK_URL)
+                    except Exception as e:
+                        logging.info("打工成功")
+                        # logging.info("打工动作执行完成")
+
+                except Exception as e:
+                    logging.error(f"定位或点击停止广告按钮时出错: {e}")
+                    choice = input("点击停止广告按钮失败，是否重新进行打工？(输入 y 重新打工，其他任意键退出): ")
+                    if choice.lower() == 'y':
+                        perform_work(driver, username, WORK_URL)
+
+    except Exception as e:
+        # 没有找到打工按钮，可能处于冷却期
+        logging.info("未找到打工按钮，可能处于冷却期")
--- tsdm_sign_tools.py
+++ tsdm_sign_tools.py
@@ -1,76 +1,113 @@
 import os
 import time
 import shutil
+import logging
 from browser_driver import setup_driver
-from config_handler import load_config
+from config_handler import load_config, save_config
 from sign_work import perform_sign, perform_work, calculate_work_time
 from scheduled_task import create_login_startup_task, create_scheduled_task
 
-LOGIN_URL = 'https://www.tsdm39.com/member.php?mod=logging&action=login'
+# 配置日志
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
 SIGN_URL = 'https://www.tsdm39.com/plugin.php?id=dsu_paulsign:sign'
 WORK_URL = 'https://www.tsdm39.com/plugin.php?id=np_cliworkdz:work'
 
 def main():
     config = load_config()
-    expired_accounts = []  # 用于记录 cookie 过期的账号
-    last_next_work_time = None # 初始化 last_next_work_time
-    for username, account_info in config.items():
+    expired_accounts = [] # 存储过期的账号
+    account_time_list = [] # 存储账号的 next_work_time 和 last_work_time
+
+    # 从 accounts 字段获取账号信息
+    accounts = config.get('accounts', {})
+    for username, account_info in accounts.items():
+        # 检查 cookie 是否有效
         if not account_info.get("is_cookie_valid", False):
             expired_accounts.append(username)
             continue
 
         driver = None
         user_data_dir = None
+        account_time_info = {
+            "username": username,
+            "next_work_time": None,
+            "last_work_time": None
+        }
+
         try:
+            logging.info(f"开始处理账号 {username} 的任务")
             driver, user_data_dir = setup_driver(headless=True)
-            # 直接执行签到和打工操作，由浏览器判断是否需要执行
             if driver:
-                print(f"开始处理账号 {username} 的签到和打工任务")
+                logging.info(f"浏览器已启动，开始访问主页面")
                 driver.get('https://www.tsdm39.com')
+                logging.info(f"开始为账号 {username} 添加 cookie")
                 for cookie in account_info["cookies"]:
                     driver.add_cookie(cookie)
-                print(f"查看账号 {username} 的签到情况")
+                logging.info(f"cookie 添加完成，准备查看账号 {username} 的签到情况")
                 perform_sign(driver, username, SIGN_URL)
-                print(f"查看账号 {username} 的打工情况")
-                perform_work(driver, username, WORK_URL)
-                
-                # 获取下次打工时间并创建计划任务
-                driver.get(WORK_URL)
-                _, next_work_time = calculate_work_time(driver)
 
-                if next_work_time:
-                    last_next_work_time = next_work_time
+                # 执行打工操作
+                perform_work(driver, username, WORK_URL)
+                # 确保获取到 next_work_time 和 last_work_time
+                while account_time_info["next_work_time"] is None or account_time_info["last_work_time"] is None:
+                    driver.get(WORK_URL)
+                    driver.execute_script("window.setTimeout = function() {};")
+                    last_work_time, next_work_time = calculate_work_time(driver)
+
+                    if last_work_time:
+                        account_time_info["last_work_time"] = last_work_time
+                    if next_work_time:
+                        account_time_info["next_work_time"] = next_work_time
+
+                account_time_list.append(account_time_info)
+
+
         except Exception as e:
-            print(f"处理账号 {username} 时发生错误: {e}")
+            logging.error(f"处理账号 {username} 时发生错误: {e}")
         finally:
             # 关闭浏览器
             if driver:
                 try:
                     driver.quit()
-                    # print("浏览器已成功关闭")
+                    # logging.info("浏览器已成功关闭")
                 except Exception as close_error:
-                    print(f"关闭浏览器时发生错误: {close_error}")
+                    logging.error(f"关闭浏览器时发生错误: {close_error}")
             if user_data_dir and os.path.exists(user_data_dir):
                 try:
                     shutil.rmtree(user_data_dir)
+                    # logging.info("用户数据目录已删除")
                 except Exception as rm_error:
-                    print(f"删除用户数据目录时出错: {rm_error}")
+                    logging.error(f"删除用户数据目录时出错: {rm_error}")
 
     # 提示 cookie 过期的账号
     if expired_accounts:
-        print("以下账号的 cookie 已过期，请重新登录获取新的 cookie:")
+        logging.info("以下账号的 cookie 已过期，请重新登录获取新的 cookie:")
         for account in expired_accounts:
-            print(account)
+            logging.info(account)
 
-    # 按最后一个账号的时间创建计划任务
-    if last_next_work_time:
-        print(f"创建下次打工计划任务，预计在 {last_next_work_time.strftime('%H:%M:%S')} 后执行")
-        create_scheduled_task(last_next_work_time)
+    # 输出每个账号的打工时间信息
+    for info in account_time_list:
+        username = info["username"]
+        last_work_time = info["last_work_time"].strftime('%H:%M:%S')
+        next_work_time = info["next_work_time"].strftime('%H:%M:%S')
+        logging.info(f"账号 {username} 在 {last_work_time} 打工完成，下次可打工时间 {next_work_time}")
 
-    print("尝试创建开机自动启动任务")
+    # 找出最迟的 next_work_time
+    latest_next_work_time = None
+    for info in account_time_list:
+        if info["next_work_time"]:
+            if latest_next_work_time is None or info["next_work_time"] > latest_next_work_time:
+                latest_next_work_time = info["next_work_time"]
+
+    # 按最迟的时间创建计划任务
+    if latest_next_work_time:
+        logging.info(f"创建下次打工计划任务，预计在 {latest_next_work_time.strftime('%H:%M:%S')} 后执行")
+        create_scheduled_task(latest_next_work_time)
+
+    logging.info("尝试创建开机自动启动任务")
     create_login_startup_task()
-    print("创建计划任务可在任务计划程序中查看")
-    print("所有任务完成，程序将在 5 秒后自动退出...")
+    logging.info("创建计划任务可在任务计划程序中查看")
+    logging.info("所有任务完成，程序将在 5 秒后自动退出...")
     time.sleep(5)  # 等待 5 秒自动退出
 
 if __name__ == "__main__":
